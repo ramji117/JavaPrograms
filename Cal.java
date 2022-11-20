c4a8f6f2e90f2426ca382f0bc72887b92b40809e
ramji117 <ramjirao11code@gmail.com>
2022-11-20 10:09:50 +0530

    first commit

---
+++ Cal.java
@@ -0,0 +1,11 @@
+package com.tel.cal;
+
+public class Cal
+{
+
+public static void main(String []args)
+{
+  System.out.println("Yes I Will Suceed One Day");
+
+}
+}
---
+++ Java.java
@@ -0,0 +1,8 @@
+class Java
+{
+
+public static void main(String[]args)
+{
+   System.out.println("Hello world I Am Learning Git And GitHub Jolly");
+{
+}
